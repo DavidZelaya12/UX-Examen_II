96a02e9d82afad4db40a54cc7d3187884b815654
DavidZelaya12 <davidzelaya06.isj@gmail.com>
2024-12-11 08:47:29 -0600

    Subida inicial

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+
+function App() {
+
+    return (
+        <div className="App">
+            (
+            <h1>Hola Mundo</h1>
+            )
+            <ToastContainer closeButton={false} />
+        </div>
+    );
+}
+
+export default App;
